c61ef568f5ffafd87272ebc77f9ad3a7eae6933c
Amrykid <amrykid@gmail.com>
2013-03-26 10:10:53 -0500

    Streaming works. Volume is implemented.
    Signed-off-by: Amrykid <amrykid@gmail.com>

--- js/core.js
+++ js/core.js
@@ -1,6 +1,8 @@
 $(function () {
 	console.log('Hello there! <3 https://github.com/Amrykid/HanasuOnline');
 });
+var IsPlaying = false;
+var Player = "";
 
 function changeVolume(volumeValue){
 	if (volumeValue < 33){
@@ -10,6 +12,8 @@ function changeVolume(volumeValue){
 	} else if (volumeValue > 66){
 		$('#volumeIcon').attr('class', 'icon-volume-up');
 	}
+	
+	$("#jquery_jplayer").jPlayer("volume", volumeValue / 100);
 }
 
 function notify() {
@@ -32,5 +36,46 @@ function notify() {
 		window.webkitNotifications.requestPermission();
 	}
 }
+function initializeApp() {
+    //any important starting procedures, we can put here.
+
+    $.getScript("js/jplayer/jquery.jplayer.min.js", function (data, textStatus, jqxhr) {
+            $("#jquery_jplayer").jPlayer({
+                swfPath: "/js/jplayer",
+				solution:"html, flash",
+				supplied: "mp3",
+				playing: function(e) {
+					setPlayStatus(true);
+				},
+				ended: function(e) {
+					setPlayStatus(false);
+				},
+				error: function(event) {
+					alert(event.jPlayer.error.type);
+				}
+            });
+			Player = $("#jquery_jplayer");
+        });
+	$("#controlPlayPause").click(function() {
+		if (IsPlaying) {
+			$("#jquery_jplayer").jPlayer("pause");
+		} else {
+			$("#jquery_jplayer").jPlayer("setMedia", { mp3: "http://174.127.103.99:443/;stream/1" });
+            $("#jquery_jplayer").jPlayer("play");
+		}
+	});
+}
+function togglePlayStatus() {
+	setPlayStatus(!IsPlaying);
+}
+function setPlayStatus(value) {
+	IsPlaying = value;
+	$("#controlPlayPause").attr("class", (IsPlaying ? "icon-pause" : "icon-play"));
+}
+ 
+		
+$(document).ready(function () {
+	initializeApp();
+});
 
 //notify();
